ee90b0e4c0d1d4323e3239d0aa868455355c9176
beebop <jmscshipp202@gmail.com>
2026-02-28 17:05:11 -0600

    working on note css

--- src/app/Note.tsx
+++ src/app/Note.tsx
@@ -1,18 +1,33 @@
 
+import { Background } from "@react-navigation/elements";
 import styles from "./page.module.css";
 
-type NoteProps = {
+
+export type NoteProps = {
+    color : string;
     message : string;
+    sender: string;
 }
 
-export default function Note({message} : NoteProps) {
-      return (
+export default function Note({color, message, sender} : NoteProps) {
+      
+    
+    const headColor= {
+        backgroundColor: `rgba(${color.r}, ${color.g}, ${color.b}, 1)`
+    };
+
+    const senderColor= {
+        backgroundColor: `rgba(${color.r}, ${color.g}, ${color.b}, 0.2)`
+    };
+
+    return (
         <div className={styles.note}>
             <div className={styles.pin}>
-                <div className={styles.pinHead}></div>
+                <div style={headColor} className={styles.pinHead}></div>
                 <div className={styles.pinBody}></div>
                 <div className={styles.pinShadow}></div>
             </div>
-            <p className={styles.noteText}>{message}</p>
+            <p className={styles.text}>{message}</p>
+            <p style={headColor} className={styles.senderText}>{`-` + sender}</p>
         </div>);
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Note from "./Note"
+
 export default function Home() {
   return (
     <div className={`${styles.page} ${styles.filledBorder}`}>
       <div className={styles.title}>
         <p>Page title here!</p>
       </div>
-    <Note message="this is the note I'm leaving!! I'm putting it in the props!"></Note>
+    <Note color="black" message="this is the note I'm leaving!! I'm putting it in the props!" sender="james"></Note>
+    <Note color="orange" message="another note! this one is shorter" sender="james"></Note>
     </div>
   );
 }

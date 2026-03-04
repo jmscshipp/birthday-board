8b427f0526de78f36422f520ac086e8743b0b6a9
beebop <jmscshipp202@gmail.com>
2026-03-04 14:32:17 -0600

    updated bulletin frame border

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,17 +3,15 @@ import Note from "./Note"
 import Picture from "./Picture"
 
 export default function Home() {
-
   return (
-    <div className={`${styles.page} ${styles.filledBorder}`}>
-      <div className={styles.title}>
-        <p>Page title here!</p>
-      </div>
-      <Note color="black" message="this is the note I'm leaving!! I'm putting it in the props!" sender="james"></Note>
-      <Picture image="/images/botanic-garden-horizontal.jpg" sender=""></Picture>
-      <Note color="orange" message="another note! this one is shorter" sender="james"></Note>
-      <Picture image="/images/botanic-garden-vertical.jpg" sender=""></Picture>
-    
+    <div className={styles.page}>
+        <div className={styles.title}>
+          <p>Page title here!</p>
+        </div>
+        <Note color="black" message="this is the note I'm leaving!! I'm putting it in the props!" sender="james"></Note>
+        <Picture image="/images/botanic-garden-horizontal.jpg" sender=""></Picture>
+        <Note color="orange" message="another note! this one is shorter" sender="james"></Note>
+        <Picture image="/images/botanic-garden-vertical.jpg" sender=""></Picture>
     </div>
   );
 }

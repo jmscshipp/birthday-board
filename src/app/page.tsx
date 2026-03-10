ebfa6d0d2bcf16fdabe9d8cae4afec7469df8905
James Shipp <jmscshipp202@gmail.com>
2026-03-10 10:08:46 -0500

    spawning notes from hardcoded data and arranging layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,26 +2,57 @@ import styles from './page.module.css';
 import Note from '../components/Note/Note';
 import Picture from '../components/Picture/Picture';
 
+type NoteData = {
+    color: string;
+    message: string;
+    sender: string;
+};
+
 export default function Home() {
+    const testData: NoteData[] = [
+        {
+            color: 'black',
+            message: "this is the note I'm leaving!! I'm putting it in the props!",
+            sender: 'howard',
+        },
+        {
+            color: 'red',
+            message: 'another note! this one is shorter',
+            sender: 'james',
+        },
+        {
+            color: 'orange',
+            message:
+                "this not is very important to me and so I'm gonna  make it extra long to fill the space yes.",
+            sender: 'Lisa',
+        },
+        {
+            color: 'green',
+            message: 'random note. I like to keep things brief',
+            sender: 'thomas',
+        },
+    ];
+
     return (
         <div className={styles.page}>
             <div className={styles.title}>
                 <p>Page title here!</p>
             </div>
-            <Note
-                color="black"
-                message="this is the note I'm leaving!! I'm putting it in the props!"
-                sender="james"
-                placement="right"
-            ></Note>
-            <Picture image="/images/botanic-garden-horizontal.jpg" sender=""></Picture>
-            <Note
-                color="orange"
-                message="another note! this one is shorter"
-                sender="james"
-                placement="left"
-            ></Note>
-            <Picture image="/images/botanic-garden-vertical.jpg" sender=""></Picture>
+            <div>
+                {testData.map((note, index) => {
+                    return (
+                        <Note
+                            key={index}
+                            color={note.color}
+                            message={note.message}
+                            sender={note.sender}
+                            placement={index % 2 == 0 ? 'left' : 'right'}
+                        ></Note>
+                    );
+                })}
+            </div>
         </div>
     );
 }
+
+//<Picture image="/images/botanic-garden-vertical.jpg" sender=""></Picture>

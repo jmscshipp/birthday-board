b7bb81418649f8bf14b489d92e296ddbf8588a0f
James Shipp <jmscshipp202@gmail.com>
2026-03-15 19:41:07 -0500

    modal font test

--- src/components/NoteModal/NoteModal.tsx
+++ src/components/NoteModal/NoteModal.tsx
@@ -19,7 +19,7 @@ export default function NoteModal({ isOpen, onClose }: NoteModalProps) {
                     </div>
                 </div>
                 <div className={styles.noteContainer}>
-                    <p>This is a simple note modal.</p>
+                    <p>this is the note I&lsquo;m leaving!! I&lsquo;m putting it in the props!</p>
                 </div>
                 <button className={styles.pinButton} disabled={true}>
                     All done, pin it!

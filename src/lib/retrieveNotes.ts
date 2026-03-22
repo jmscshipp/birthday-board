471da56ad9c6dd310004a59587b36fb34186911a
James Shipp <jmscshipp202@gmail.com>
2026-03-22 15:49:27 -0500

    vercel fix

--- src/lib/retrieveNotes.ts
+++ src/lib/retrieveNotes.ts
@@ -2,14 +2,20 @@ import { collection, getDocs } from 'firebase/firestore';
 import { db } from '@/lib/firebase';
 
 export const getNotes = async () => {
-    const snapshot = await getDocs(collection(db, 'notes'));
-    return snapshot.docs.map((doc) => ({
-        id: doc.id,
-        kind: 'note' as const,
-        color: doc.data().color,
-        message: doc.data().message,
-        sender: doc.data().sender,
-        margin: doc.data().margin,
-        createdAt: doc.data().createdAt.toDate(),
-    }));
+    try {
+        const snapshot = await getDocs(collection(db, 'notes'));
+        console.log('notes snapshot size:', snapshot.size);
+        return snapshot.docs.map((doc) => ({
+            id: doc.id,
+            kind: 'note' as const,
+            color: doc.data().color,
+            message: doc.data().message,
+            sender: doc.data().sender,
+            margin: doc.data().margin,
+            createdAt: doc.data().createdAt.toDate(),
+        }));
+    } catch (error) {
+        console.error('Error fetching notes:', error);
+        return [];
+    }
 };

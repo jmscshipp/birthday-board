2b083e43f9493bd47fd8a16c2c32fc322703ac51
James Shipp <jmscshipp202@gmail.com>
2026-03-22 20:19:45 -0500

    font wasn't actually set up right..

--- src/components/Note/Note.tsx
+++ src/components/Note/Note.tsx
@@ -17,19 +17,10 @@ export default function Note({ color, font, message, sender, placement, margin }
         return styles.placeCenter;
     };
 
-    const getFontStyle = () => {
-        if (font === 'var(--font-kalam)') return styles.kalam;
-        if (font === 'var(--font-dancing-script)') return styles.dancingScript;
-        if (font === 'var(--font-comic-relief)') return styles.comicRelief;
-        if (font === 'var(--font-new-rocker)') return styles.newRocker;
-        if (font === 'var(--font-jersey-10)') return styles.jersey10;
-        return styles.geistSans;
-    };
-
     return (
         <div
             style={{ marginTop: margin + 'px' }}
-            className={`${getFontStyle()} ${getPlacementStyle()}`}
+            className={`${'fontFamily: ' + font} ${getPlacementStyle()}`}
         >
             <div className={styles.container}>
                 <Pin color={color}></Pin>
